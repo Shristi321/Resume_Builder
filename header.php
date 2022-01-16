c57dd05a4e8f1d05c6cb76534cf734d983f5c678
Shristi321 <59893870+Shristi321@users.noreply.github.com>
2022-01-16 13:41:29 -0600

    Home page
    Simple first page created

---
+++ header.php
@@ -0,0 +1,18 @@
+<div class="header">
+    <a href = "index.php"> <img id="logo" src="img/logo.png"/></a>
+    <p id="logo_desc">Next step to your dream job...</p>
+  
+</div>
+
+<div class="menubar">
+    <ul id="menu">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="templates.php">Templates</a></li>
+        <li><a href="signup.php">Signup</a></li>
+        <li><a href="my_account.php">My Resumes</a></li>
+
+    </ul>
+
+</div>
+
+
--- index.php
+++ index.php
@@ -2,8 +2,35 @@
 <html>
 <head>
 	<title>Resume Builder</title>
+    <link rel="stylesheet" href="styles/style.css" media="all"/>
 </head>
 <body>
-Start your resume!!
+    <?php
+    include("header.php");
+    ?>
+
+<div class="main_content">
+    <div class="content">
+        <p id="main_content_title">Build the Best Resume</p>
+        <p id="main_content_desc">
+            Our resume builder will help you make your prefect resume with options to change templates for free.<br> Est, tristique molestie commodo dignissim rutrum natoque lacinia iaculis amet elementum etiam phasellus lacus parturient donec sed sit justo parturient malesuada porta placerat habitant dolor senectus senectus est arcu tincidunt.
+        </p>
+       
+        <div class="button_center">
+            <button class="center button" id="build_resume_button">Build my Resume</button>
+        </div>
+        
+    </div>
+    <div class="content_2">
+        <img id="resume_1" src="img/resume_1.jpeg"/>
+    </div>
+</div>
+
+<div class="footer">
+  <p>&copy;Copyright 2022 resumebuilder.shristichapagain.com</p>
+</div>
+
+
+
 </body>
 </html>
